d3cecf703fae555af1d3a09f38190d1d5fe46bcb
gshirato <gshirato@uni-bonn.de>
2022-11-26 17:14:37 +0100

    add env to gitignore

--- python/pass_network_table.py
+++ python/pass_network_table.py
@@ -1,9 +1,10 @@
 import tabula
 
 def parse_network_table(pdf_path, page):
-    df = tabula.read_pdf(pdf_path, pages=page, stream=True)[0].reset_index(drop=True).iloc[5:, :-2]
-    df = df.dropna(subset=['Unnamed: 0'])
+    df = tabula.read_pdf(pdf_path, pages=page, stream=True)[0].reset_index(drop=True).iloc[:, :-2]
+    df = df.dropna(subset=['Unnamed: 0']).iloc[1:, :]
     df = df.drop('Unnamed: 2', axis=1)
+
     players = df['Unnamed: 1'].tolist()
     df.columns = ['#', 'name', *players]
     return df.reset_index(drop=True)
